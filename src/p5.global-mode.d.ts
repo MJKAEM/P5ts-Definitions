7fa40cba55e1d132006bdf925cb1dcf86a110a59
M Kuan <Martinokuan@gmail.com>
2016-11-02 15:06:26 -0700

    Add Data -> String Functions

--- src/p5.global-mode.d.ts
+++ src/p5.global-mode.d.ts
@@ -4021,6 +4021,197 @@ declare function unhex(n: string[]): number[];
 // Data -> String Functions
 //
 
+/**
+ * Combines an array of Strings into one String, each separated by the
+ * character(s) used for the separator parameter. To join arrays of ints or
+ * floats, it's necessary to first convert them to Strings using nf() or nfs().
+ * 
+ * @param {string[]} list array of Strings to be joined
+ * @param {string} separator String to be placed between each item
+ */
+declare function join(list: string[], separator: string): string;
+
+/**
+ * If there are groups (specified by sets of parentheses) in the regular
+ * expression, then the contents of each will be returned in the array. Element
+ * [0] of a regular expression match returns the entire matching string, and
+ * the match groups start at element [1] (the first group is [1], the
+ * second [2], and so on).
+ * 
+ * @param {string} str the String to be searched
+ * @param {string} regexp the regexp to be used for matching
+ */
+declare function match(str: string, regexp: string): string;
+
+/**
+ * This function is used to apply a regular expression to a piece of text, and
+ * return a list of matching groups (elements found inside parentheses) as a
+ * two-dimensional String array. If there are no matches, a null value will be
+ * returned. If no groups are specified in the regular expression, but the
+ * sequence matches, a two dimensional array is still returned, but the second
+ * dimension is only of length one.
+ * 
+ * To use the function, first check to see if the result is null. If the result
+ * is null, then the sequence did not match at all. If the sequence did match,
+ * a 2D array is returned.
+ * 
+ * If there are groups (specified by sets of parentheses) in the regular
+ * expression, then the contents of each will be returned in the array.
+ * Assuming a loop with counter variable i, element [i][0] of a regular
+ * expression match returns the entire matching string, and the match groups
+ * start at element [i][1] (the first group is [i][1], the second [i][2], and
+ * so on).
+ * 
+ * @param {string} str the String to be searched
+ * @param {string} reg the regexp to be used for matching
+ */
+declare function matchAll(str: string, reg: string): string[];
+
+/**
+ * Utility function for formatting numbers into strings. There are two
+ * versions: one for formatting floats, and one for formatting ints. The values
+ * for the digits, left, and right parameters should always be positive
+ * integers.
+ * 
+ * @param {number} num the Number to format
+ * @param {number} left number of digits to the left of the decimal point
+ * @param {number} right number of digits to the right of the decimal point
+ */
+declare function nf(num: number, left?: number, right?: number): string;
+
+/**
+ * Utility function for formatting numbers into strings. There are two
+ * versions: one for formatting floats, and one for formatting ints. The values
+ * for the digits, left, and right parameters should always be positive
+ * integers.
+ * 
+ * @param {number[]} num the Number to format
+ * @param {number} left number of digits to the left of the decimal point
+ * @param {number} right number of digits to the right of the decimal point
+ */
+declare function nf(num: number[], left?: number, right?: number): string[];
+
+/**
+ * Utility function for formatting numbers into strings and placing appropriate
+ * commas to mark units of 1000. There are two versions: one for formatting
+ * ints, and one for formatting an array of ints. The value for the right
+ * parameter should always be a positive integer.
+ * 
+ * @param {number} num the Number to format
+ * @param {number} right number of digits to the right of the decimal point
+ */
+declare function nfc(num: number, right?: number): string;
+
+/**
+ * Utility function for formatting numbers into strings and placing appropriate
+ * commas to mark units of 1000. There are two versions: one for formatting
+ * ints, and one for formatting an array of ints. The value for the right
+ * parameter should always be a positive integer.
+ * 
+ * @param {number[]} num the Number to format
+ * @param {number} right number of digits to the right of the decimal point
+ */
+declare function nfc(num: number[], right?: number): string[];
+
+/**
+ * Utility function for formatting numbers into strings. Similar to nf() but
+ * puts a "+" in front of positive numbers and a "-" in front of negative
+ * numbers. There are two versions: one for formatting floats, and one for
+ * formatting ints. The values for left, and right parameters should always be
+ * positive integers.
+ * 
+ * @param {number} num the Number to format
+ * @param {number} left number of digits to the left of the decimal point
+ * @param {number} right number of digits to the right of the decimal point
+ */
+declare function nfp(num: number, left?: number, right?: number): string;
+
+/**
+ * Utility function for formatting numbers into strings. Similar to nf() but
+ * puts a "+" in front of positive numbers and a "-" in front of negative
+ * numbers. There are two versions: one for formatting floats, and one for
+ * formatting ints. The values for left, and right parameters should always be
+ * positive integers.
+ * 
+ * @param {number[]} num the Number to format
+ * @param {number} left number of digits to the left of the decimal point
+ * @param {number} right number of digits to the right of the decimal point
+ */
+declare function nfp(num: number[], left?: number, right?: number): string[];
+
+/**
+ * Utility function for formatting numbers into strings. Similar to nf() but
+ * puts a " " (space) in front of positive numbers and a "-" in front of
+ * negative numbers. There are two versions: one for formatting floats, and one
+ * for formatting ints. The values for the digits, left, and right parameters
+ * should always be positive integers.
+ * 
+ * @param {number} num the Number to format
+ * @param {number} left number of digits to the left of the decimal point
+ * @param {number} right number of digits to the right of the decimal point
+ */
+declare function nfs(num: number, left?: number, right?: number): string;
+
+/**
+ * Utility function for formatting numbers into strings. Similar to nf() but
+ * puts a " " (space) in front of positive numbers and a "-" in front of
+ * negative numbers. There are two versions: one for formatting floats, and one
+ * for formatting ints. The values for the digits, left, and right parameters
+ * should always be positive integers.
+ * 
+ * @param {number[]} num the Number to format
+ * @param {number} left number of digits to the left of the decimal point
+ * @param {number} right number of digits to the right of the decimal point
+ */
+declare function nfs(num: number[], left?: number, right?: number): string;
+
+/**
+ * The split() function maps to String.split(), it breaks a String into pieces
+ * using a character or string as the delimiter. The delim parameter specifies
+ * the character or characters that mark the boundaries between each piece. A
+ * String[] array is returned that contains each of the pieces.
+ * 
+ * The splitTokens() function works in a similar fashion, except that it splits
+ * using a range of characters instead of a specific character or sequence.
+ * 
+ * @param {string} value the String to be split
+ * @param {string} delim the String used to separate the data
+ */
+declare function split(value: string, delim: string): string[];
+
+/**
+ * The splitTokens() function splits a String at one or many character
+ * delimiters or "tokens." The delim parameter specifies the character or
+ * characters to be used as a boundary.
+ * 
+ * If no delim characters are specified, any whitespace character is used to
+ * split. Whitespace characters include tab (\t), line feed (\n), carriage
+ * return (\r), form feed (\f), and space.
+ * 
+ * @param {string} value the String to be split
+ * @param {string} delim list of individual Strings that will be used as
+ * 		separators
+ */
+declare function splitTokens(value: string,delim?: string): string[];
+
+/**
+ * Removes whitespace characters from the beginning and end of a String. In
+ * addition to standard whitespace characters such as space, carriage return,
+ * and tab, this function also removes the Unicode "nbsp" character.
+ * 
+ * @param {string} str a String or Array of Strings to be trimmed
+ */
+declare function trim(str: string): string;
+
+/**
+ * Removes whitespace characters from the beginning and end of a String. In
+ * addition to standard whitespace characters such as space, carriage return,
+ * and tab, this function also removes the Unicode "nbsp" character.
+ * 
+ * @param {string[]} str a String or Array of Strings to be trimmed
+ */
+declare function trim(str: string[]): string[];
+
 //
 // Lights, Camera -> Camera
 //
