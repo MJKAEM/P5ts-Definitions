35417e95070cf30bc8bdb5768c2dee99fda64557
M Kuan <Martinokuan@gmail.com>
2017-01-08 23:07:31 -0800

    Provide more feedback to the user in the build.py file.

--- build.py
+++ build.py
@@ -1,17 +1,22 @@
 import os
 
+OUTPUT_NAME_DEFAULT = "p5.complete.d.ts"
+
 def main():
     """
     Main function
     """
+    print("Building files into " + OUTPUT_NAME_DEFAULT)
     inputdir = "src"
     classfilesdir = inputdir + "/class_files"
 
-    with open("p5.complete.d.ts", 'w') as outputfile:
+    with open(OUTPUT_NAME_DEFAULT, 'w') as outputfile:
         printtooutput(inputdir + "/p5.d.ts", outputfile)
         printtooutput(inputdir + "/p5.global-mode.d.ts", outputfile)
         for filename in os.listdir(classfilesdir):
             printtooutput(classfilesdir + "/" + filename, outputfile)
+
+    print("Success! The built file can be found in " + OUTPUT_NAME_DEFAULT + "!")
 
 def printtooutput(filename, output):
     """
@@ -19,6 +24,7 @@ def printtooutput(filename, output):
     """
 
     with open(filename) as inputfile:
+        print("Reading from " + filename)
         line = inputfile.readline()
         while line != "":
             output.write(line)
